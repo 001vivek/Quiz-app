9169290b53ebd038d06337339d12a8080cab511a
vivek rakhunde <dev.vivek@mejidigital.in>
2024-07-03 18:30:28 +0530

    quiz app setup

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,41 @@
+import { useCallback, useState } from "react";
+import QUESTIONS from "../questions";
+import Question from "./Question";
+import Summery from "./Summery";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex =userAnswers.length
+
+  const isQuizcomplted = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setUserAnswers((prevState) => {
+        return [...prevState, selectedAnswer];
+      });
+    },
+
+    []
+  );
+  const hanldeSkipAnswer = useCallback(() => {
+    handleSelectAnswer(null);
+  }, [handleSelectAnswer]);
+  if (isQuizcomplted) {
+    return (
+      <Summery userAnswers={userAnswers}  />
+    );
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={hanldeSkipAnswer}
+      />
+    </div>
+  );
+}
